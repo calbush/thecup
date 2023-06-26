c09eb0846fa2783d820f4e883a8706e31ed1bc22
Caleb Bush <calebmatthewbush@gmail.com>
2023-06-26 00:22:14 -0500

    Add functionality for fetching data from TMDB

--- src/App.js
+++ src/App.js
@@ -1,70 +1,59 @@
 import Form from "./components/Form";
-import { isEqual } from "lodash";
-//const fetch = require('node-fetch')
+import MovieCard from "./components/MovieCard";
+import { captureFormData, compareParamsHelper } from "./utils/captureFormData";
+import { movieRandomizer } from "./utils/movieRandomizer";
+import { useState } from "react";
 
-//const matchingFilms = []
 
-let queryParams = {
-  'watchRegion': 'US',
-  'providerIds': [],
-  'genreIds': [],
-}
+let queryParams = {}
 
 let retrievedMovies = []
 
-const handleClick = () => {
-  //Make a new object with form info. IF it's different from queryParams, make another API call.
-    const newQueryParams = {
-      'watchRegion': 'US',
-      'providerIds': [],
-      'genreIds': [],
-    }
-    const genreDiv = document.getElementById('genre-checkboxes-div')
-    const genreCheckboxes = genreDiv.querySelectorAll('input')
-    genreCheckboxes.forEach(genreCheckbox => {
-      if(genreCheckbox.checked){
-        newQueryParams.genreIds.push(genreCheckbox.id)
-      }
-    })
-    const providerDiv = document.getElementById('provider-checkboxes-div')
-    const providerCheckboxes = providerDiv.querySelectorAll('input')
-    providerCheckboxes.forEach(providerCheckbox => {
-      if(providerCheckbox.checked){
-        newQueryParams.providerIds.push(providerCheckbox.id)
-      }
-    })
-
-    if (isEqual(newQueryParams, queryParams)){
-      console.log('They\'re the same!')
-    } else {
-      queryParams = {...newQueryParams}
-      console.log('They\'re different!')
-      retrieveMovies()
-    }
-}
-
-const retrieveMovies = () => {
-  const url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&region=US&sort_by=popularity.desc&watch_region=${queryParams.watchRegion}&with_genres=${queryParams.genreIds.join('|')}&with_watch_providers=${queryParams.providerIds.join('|')}`
-  const options = {
-    method : 'GET',
-    headers : {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNGQzYTEyMmUxNjYxNDkwODk2MjhhZmI4MzZhNWEzMSIsInN1YiI6IjY0NmI3NzA5MmJjZjY3MDEzODk1ZjAyNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cUF2N4BlYPIZ6e6AhtgAlQ0LD9ZPGqvlSWfONJXH0hc'
-    }
-  }
-
-  fetch(url, options)
-  .then(res => res.json())
-  .then(json => {
-    retrievedMovies = [...json.results]
-    console.log(retrievedMovies)
-  })
-}
 
 function App() {
+
+const [currentMovie, setCurrentMovie] = useState('')
+
+const handleClick = () => {
+  const newParams = captureFormData()
+  const paramsAreSame = compareParamsHelper(newParams, queryParams)
+  if (!paramsAreSame){
+    queryParams = {...newParams}
+    const url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&region=US&sort_by=popularity.desc&watch_region=${newParams.watchRegion}&with_genres=${newParams.genreIds.join('|')}&with_watch_providers=${newParams.providerIds.join('|')}`
+    const options = {
+      method : 'GET',
+      headers : {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYjk1ZDBkOTBjYjY0NWM5ODI4NzQ5OGQ4YjAzYmU0NiIsInN1YiI6IjY0NmI3NzA5MmJjZjY3MDEzODk1ZjAyNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.z-VMPfC37YRrc70K_jFz27jVgd-_bbWlnUSJHRO4jww'
+      }
+    }
+  
+    fetch(url, options)
+    .then(res => res.json())
+    .then(json => {
+      //After successfully calling the TMDB API, we need to store our response and select a random movie
+      retrievedMovies = [...json.results]
+      const randomIndex = movieRandomizer(retrievedMovies)
+      const randomlySelectedMovie = retrievedMovies[randomIndex]
+      retrievedMovies.splice(randomIndex, 1)
+      setCurrentMovie({...randomlySelectedMovie})
+      console.log(currentMovie)
+    })
+    .catch(err => console.error('error:' + err))
+  } else {
+      const randomIndex = movieRandomizer(retrievedMovies)
+      const randomlySelectedMovie = retrievedMovies[randomIndex]
+      retrievedMovies.splice(randomIndex, 1)
+      setCurrentMovie({...randomlySelectedMovie})
+      console.log(currentMovie)
+  }
+  
+}
+
   return (
     <div className="App">
       <Form handleClick={handleClick}/>
+      <MovieCard movie={currentMovie}/>
     </div>
   );
 }
