51de87b1cac56d90181f0140ef2b8fe022c683af
Caleb Bush <calebmatthewbush@gmail.com>
2023-07-03 00:12:52 -0500

    Fix bug causing movie to only appear every second click

--- src/App.js
+++ src/App.js
@@ -1,68 +1,82 @@
 
 import Form from "./components/Form";
 import MovieCard from "./components/MovieCard";
-//import { movieRandomizer } from "./utils/movieRandomizer";
-import { useEffect, useState, useRef } from "react";
+import { useState, useRef } from "react";
 import { movieRandomizer } from "./utils/movieRandomizer";
+import captureFormData from "./utils/captureFormData";
+import { isEqual } from "lodash";
 
 function App() {
-
-let retrievedMovies = useRef([])
-const [currentMovie, setCurrentMovie] = useState('')
-const [providerIds, setProviderIds] = useState([])
-const [genreIds, setGenreIds] = useState([])
-const watchRegion = 'US'
-
-const genreJSON = JSON.stringify(genreIds)
-const providerJSON = JSON.stringify(providerIds)
-
-
-//If state changes we make an api call with the new parameters
-useEffect(() => {
-    const url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&region=US&sort_by=popularity.desc&watch_region=${watchRegion}&with_genres=${genreIds.join('|')}&with_watch_providers=${providerIds.join('|')}`
-    const options = {
-      method : 'GET',
-      headers : {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYjk1ZDBkOTBjYjY0NWM5ODI4NzQ5OGQ4YjAzYmU0NiIsInN1YiI6IjY0NmI3NzA5MmJjZjY3MDEzODk1ZjAyNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.z-VMPfC37YRrc70K_jFz27jVgd-_bbWlnUSJHRO4jww'
-      }
+  const retrievedMovies = useRef([])
+  const [currentMovie, setCurrentMovie] = useState('')
+  let queryParameters = useRef({})
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYjk1ZDBkOTBjYjY0NWM5ODI4NzQ5OGQ4YjAzYmU0NiIsInN1YiI6IjY0NmI3NzA5MmJjZjY3MDEzODk1ZjAyNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.z-VMPfC37YRrc70K_jFz27jVgd-_bbWlnUSJHRO4jww'
     }
-    fetch(url, options)
-    .then(res => res.json())
-    .then(json => {
-      retrievedMovies.current = [...json.results]
-      console.log('api call made')
-      console.log(retrievedMovies)
-    })
-    .catch(err => console.error('error: ' + err))
-},[genreJSON, providerJSON])
-
-const handleClick = () => {
-    const newGenreIds = []
-    const genreChecks = document.getElementById('genre-checkboxes-div')
-    const genreNodelist = genreChecks.querySelectorAll('input')
-    genreNodelist.forEach((checkbox) => {
-      if(checkbox.checked){
-        newGenreIds.push(checkbox.id)
-      }
-    })
-    const newProviderIds = []
-    const providerChecks = document.getElementById('provider-checkboxes-div')
-    const providerNodelist = providerChecks.querySelectorAll('input')
-    providerNodelist.forEach((checkbox) => {
-      if(checkbox.checked){
-        newProviderIds.push(checkbox.id)
-      }
-    })
-    setGenreIds([...newGenreIds])
-    setProviderIds([...newProviderIds])
-    //Pick a random movie, display it, then remove it from retrievedMovies
-    const movieIndex = movieRandomizer(retrievedMovies.current)
-    setCurrentMovie(retrievedMovies.current[movieIndex])
-    retrievedMovies.current.splice(movieIndex, 1)
-    console.log(retrievedMovies.current)
   }
 
+  const getMoviesForPage = (results_page_num) => {
+    const url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${results_page_num}&watch_region=US&sort_by=popularity.desc&with_genres=${queryParameters.current.genreIds.join('|')}&with_watch_providers=${queryParameters.current.providerIds.join('|')}&vote_count.gte=${queryParameters.current.popularityMin}&vote_count.lte=${queryParameters.current.popularityMax}`
+    return fetch(url, options)
+    .then(res => res.json())
+    .then(json => {
+      return json.results
+      //console.log('added to retrieved movies:' + retrievedMovies.current.length)
+    })
+    .catch(err => console.error('error: ' + err))
+  }
+
+  const handleClick = () => {
+    const newQueryParameters = captureFormData()
+    let totalPages = 0
+    const randomlyFetchedPages = new Set()
+    if(retrievedMovies.current.length === 0 || !isEqual(queryParameters.current, newQueryParameters)){
+      retrievedMovies.current = []
+      queryParameters.current = {...newQueryParameters}
+      const url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&watch_region=US&sort_by=popularity.desc&with_genres=${queryParameters.current.genreIds.join('|')}&with_watch_providers=${queryParameters.current.providerIds.join('|')}&vote_count.gte=${queryParameters.current.popularityMin}&vote_count.lte=${queryParameters.current.popularityMax}`
+      fetch(url, options)
+        .then(res => res.json())
+        .then(json => {
+          totalPages = json.total_pages
+          const promiseArray = []
+          //We don't need to randomize retrieved pages if there are < 10 total_pages
+          if (totalPages <= 10){
+            for (let pageNumber = 1; pageNumber <= totalPages; pageNumber++){
+              promiseArray.push(getMoviesForPage(pageNumber))
+            }
+          } else {
+            console.log('total pages: ' + totalPages)
+            //Get 10 random pages in the range of total_pages (TMDB doesn't allow us to retrieve > page 500)
+            while(randomlyFetchedPages.size < 10){
+              const randomNum = Math.floor(Math.random() * Math.min(totalPages, 499))
+              randomlyFetchedPages.add(randomNum)
+            }
+            randomlyFetchedPages.forEach((pageNumber) => {
+              promiseArray.push(getMoviesForPage(pageNumber))
+            })
+          }
+          Promise.all(promiseArray)
+          .then(results => {
+            retrievedMovies.current = results.flat()
+            const randomIndex = movieRandomizer(retrievedMovies.current)
+            setCurrentMovie(retrievedMovies.current[randomIndex])
+            retrievedMovies.current.splice(randomIndex, 1)
+          })
+        })
+        .catch(err => console.error('error:' + err));
+    } else {
+      const randomIndex = movieRandomizer(retrievedMovies.current)
+      setCurrentMovie(retrievedMovies.current[randomIndex])
+      retrievedMovies.current.splice(randomIndex, 1)
+    }
+    
+
+
+
+  }
   return (
     <div className="App">
       <Form handleClick={handleClick}/>
@@ -72,4 +86,7 @@ const handleClick = () => {
 
   }
 
+
+
   export default App
+
