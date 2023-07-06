dbdadfc85bf08c36ce06e5962794309d7a5a7c5c
Caleb Bush <calebmatthewbush@gmail.com>
2023-07-06 13:01:47 -0500

    Restructure component with helper functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 
-import Form from "./components/Form";
+import { Form } from "./components/Form";
 import MovieCard from "./components/MovieCard";
 import { useState, useRef } from "react";
 import { movieRandomizer } from "./utils/movieRandomizer";
-import captureFormData from "./utils/captureFormData";
 import { isEqual } from "lodash";
 
 function App() {
@@ -19,7 +18,7 @@ function App() {
   }
 
   const getMoviesForPage = (results_page_num) => {
-    const url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${results_page_num}&watch_region=US&sort_by=popularity.desc&with_genres=${queryParameters.current.genreIds.join('|')}&with_watch_providers=${queryParameters.current.providerIds.join('|')}&vote_count.gte=${queryParameters.current.popularityMin}&vote_count.lte=${queryParameters.current.popularityMax}`
+    const url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${results_page_num}&watch_region=US&sort_by=popularity.desc&with_genres=${queryParameters.current.genreIds.join('|')}&with_watch_providers=${queryParameters.current.providerIds.join('|')}&vote_count.gte=${queryParameters.current.popularityMin}&vote_count.lte=${queryParameters.current.popularityMax}&vote_average.gte=${queryParameters.current.ratingMin}&vote_average.lte=${queryParameters.current.ratingMax}`
     return fetch(url, options)
     .then(res => res.json())
     .then(json => {
@@ -29,38 +28,43 @@ function App() {
     .catch(err => console.error('error: ' + err))
   }
 
-  const handleClick = () => {
-    const newQueryParameters = captureFormData()
-    let totalPages = 0
+  const getRandomizedPages = (totalPages) => {
+    const moviePromises = []
     const randomlyFetchedPages = new Set()
-    if(retrievedMovies.current.length === 0 || !isEqual(queryParameters.current, newQueryParameters)){
+    if (totalPages <= 10){
+      for (let pageNumber = 1; pageNumber <= totalPages; pageNumber++){
+        moviePromises.push(getMoviesForPage(pageNumber))
+      }
+    } else {
+      //Get 10 random pages in the range of total_pages (TMDB doesn't allow us to retrieve > page 500)
+      while(randomlyFetchedPages.size < 10){
+        const randomNum = Math.floor(Math.random() * Math.min(totalPages, 499))
+        randomlyFetchedPages.add(randomNum)
+      }
+      randomlyFetchedPages.forEach((pageNumber) => {
+        moviePromises.push(getMoviesForPage(pageNumber))
+      })
+    }
+    return moviePromises
+  }
+
+  const handleClick = (newParams) => {
+    console.log('Old: ' , queryParameters.current)
+    console.log('New: ' , newParams)
+    if(retrievedMovies.current.length === 0 || !isEqual(queryParameters.current, newParams)){
+      console.log('different')
       retrievedMovies.current = []
-      queryParameters.current = {...newQueryParameters}
-      const url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&watch_region=US&sort_by=popularity.desc&with_genres=${queryParameters.current.genreIds.join('|')}&with_watch_providers=${queryParameters.current.providerIds.join('|')}&vote_count.gte=${queryParameters.current.popularityMin}&vote_count.lte=${queryParameters.current.popularityMax}`
+      queryParameters.current = JSON.parse(JSON.stringify(newParams)) //{...newParams}
+      const url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&watch_region=US&sort_by=popularity.desc&with_genres=${queryParameters.current.genreIds.join('|')}&with_watch_providers=${queryParameters.current.providerIds.join('|')}&vote_count.gte=${queryParameters.current.popularityMin}&vote_count.lte=${queryParameters.current.popularityMax}&vote_average.gte=${queryParameters.current.ratingMin}&vote_average.lte=${queryParameters.current.ratingMax}`
       fetch(url, options)
         .then(res => res.json())
         .then(json => {
-          totalPages = json.total_pages
-          const promiseArray = []
-          //We don't need to randomize retrieved pages if there are < 10 total_pages
-          if (totalPages <= 10){
-            for (let pageNumber = 1; pageNumber <= totalPages; pageNumber++){
-              promiseArray.push(getMoviesForPage(pageNumber))
-            }
-          } else {
-            console.log('total pages: ' + totalPages)
-            //Get 10 random pages in the range of total_pages (TMDB doesn't allow us to retrieve > page 500)
-            while(randomlyFetchedPages.size < 10){
-              const randomNum = Math.floor(Math.random() * Math.min(totalPages, 499))
-              randomlyFetchedPages.add(randomNum)
-            }
-            randomlyFetchedPages.forEach((pageNumber) => {
-              promiseArray.push(getMoviesForPage(pageNumber))
-            })
-          }
-          Promise.all(promiseArray)
+          const totalPages = json.total_pages
+          const moviePromises = getRandomizedPages(totalPages)
+          Promise.all(moviePromises)
           .then(results => {
             retrievedMovies.current = results.flat()
+            console.log(retrievedMovies.current.length)
             const randomIndex = movieRandomizer(retrievedMovies.current)
             setCurrentMovie(retrievedMovies.current[randomIndex])
             retrievedMovies.current.splice(randomIndex, 1)
@@ -68,6 +72,7 @@ function App() {
         })
         .catch(err => console.error('error:' + err));
     } else {
+      console.log('same')
       const randomIndex = movieRandomizer(retrievedMovies.current)
       setCurrentMovie(retrievedMovies.current[randomIndex])
       retrievedMovies.current.splice(randomIndex, 1)
