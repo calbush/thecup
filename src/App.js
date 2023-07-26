be00facfae5ef5b4a1c3781af4cbd7e920e75343
Caleb Bush <calebmatthewbush@gmail.com>
2023-07-26 17:25:33 -0500

    Add function for making second API call to fetch movie data

--- src/App.js
+++ src/App.js
@@ -5,30 +5,58 @@ import { useState, useRef } from "react";
 import { movieRandomizer } from "./utils/movieRandomizer";
 
 function App() {
-  const retrievedMovies = useRef([])
+  const retrievedMovieIds = useRef([])
   const [currentMovie, setCurrentMovie] = useState('')
 
-  const handleClick = async (callback, oldParams, newParams) => {
-    const result = await callback(oldParams, newParams)
-    if (result) {
-      retrievedMovies.current = []
-          Promise.all(result)
-          .then(results => {
-            retrievedMovies.current = results.flat()
-            const randomIndex = movieRandomizer(retrievedMovies.current)
-            setCurrentMovie(retrievedMovies.current[randomIndex])
-            retrievedMovies.current.splice(randomIndex, 1)
-          })
-          .catch(err => console.error(err))
-    } else {
-        const randomIndex = movieRandomizer(retrievedMovies.current)
-        setCurrentMovie(retrievedMovies.current[randomIndex])
-        retrievedMovies.current.splice(randomIndex, 1)
-    }
+  async function getMovieInfo(id){
+    const movieInfo = []
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYjk1ZDBkOTBjYjY0NWM5ODI4NzQ5OGQ4YjAzYmU0NiIsInN1YiI6IjY0NmI3NzA5MmJjZjY3MDEzODk1ZjAyNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.z-VMPfC37YRrc70K_jFz27jVgd-_bbWlnUSJHRO4jww'
+      }
+    };    
+    const providerUrl = `https://api.themoviedb.org/3/movie/${id}/watch/providers`
+    const movieInfoUrl = `https://api.themoviedb.org/3/movie/${id}`
+
+    const responses = await Promise.all([fetch(providerUrl, options), fetch(movieInfoUrl, options)])
+
+    const providerData = await responses[0].json()
+    const movieData = await responses[1].json()
+    
+    movieInfo.push(providerData.results?.US)
+    movieInfo.push(movieData)
+
+    return movieInfo
   }
+
+  const handleClick = async (callback, oldParams, newParams) => {
+    const result = await callback(oldParams, newParams);
+    if (result) {
+      retrievedMovieIds.current = [];
+      try {
+        const results = await Promise.all(result);
+        retrievedMovieIds.current = results.flat().map(obj => obj?.id);
+        const randomIndex = movieRandomizer(retrievedMovieIds.current);
+        const movieInfo = await getMovieInfo(retrievedMovieIds.current[randomIndex]);
+        retrievedMovieIds.current.splice(randomIndex, 1)
+        setCurrentMovie(movieInfo)
+  
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      const randomIndex = movieRandomizer(retrievedMovieIds.current);
+      const movieInfo = await getMovieInfo(retrievedMovieIds.current[randomIndex])
+      retrievedMovieIds.current.splice(randomIndex, 1);
+      setCurrentMovie(movieInfo);
+    }
+  };
+  
     
   return (
-    <div className="App">
+    <div className="app">
       <Form handleClick={handleClick}/>
       <MovieCard movie={currentMovie}/>
     </div>
