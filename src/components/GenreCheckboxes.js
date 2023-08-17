1b9a86cf93ea499bf4130a9dbc69829babd02390
Caleb Bush <calebmatthewbush@gmail.com>
2023-08-16 20:49:25 -0500

    Remove checkboxes and replace with single div

--- src/components/GenreCheckboxes.js
+++ src/components/GenreCheckboxes.js
@@ -5,13 +5,10 @@ export default function GenreCheckboxes({genres, handleChange, genreCheckboxes, 
             <div className='tab-title'> Select your genre(s):</div>
             <div className='genre-checkboxes-container'>
                 {genres.map((genre) => (
-                        <label className='checkbox-with-label' htmlFor={genre.id}key={genre.id}>
-                            <input className='' type='checkbox' id={genre.id} name={genre.name} onChange={handleChange} checked={genreCheckboxes[genre.id]}/>
-                             {genre.name}
-                        </label>
+                    <div className={genreCheckboxes[genre.id]} key={genre.id} onClick={handleChange} id={genre.id}>{genre.name}</div>
                 ))}
             </div>
-            <div class="toggle-btns">
+            <div className="toggle-btns">
                 <button className='toggle-btn' onClick={toggleAll}>Check All</button>
                 <button className='toggle-btn' onClick={untoggleAll}>Uncheck All</button>
             </div>
