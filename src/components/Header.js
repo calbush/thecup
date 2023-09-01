0e4539797149a49ae8663b89dbd1153e1581ed7b
Caleb Bush <calebmatthewbush@gmail.com>
2023-08-31 21:38:07 -0500

    Remove logo from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,8 @@
-import logo from '../assets/solo_cup_logo.jpeg'
 
 export default function Header(){
     return (
         <header className='header'>
             <div className='header-logo'>
-                <img src={logo} alt='plastic red cup' className='main-logo'/>
                 <h1 className='header-text'>The Cup</h1>
             </div>
         </header>
