38c81b1eba0145ca7b84a7030eb8abb008208bbc
Caleb Bush <calebmatthewbush@gmail.com>
2023-09-29 16:00:45 -0500

    Add and style movie-roulette subtitle

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,10 @@ export default function Header(){
     return (
         <header className='header'>
             <div className='header-logo'>
-                <h1 className='header-text'>The Cup</h1>
+                <h1 className='header-text'>
+                    The Cup
+                    <div className='small-title'>movie-roulette</div>
+                </h1>
             </div>
         </header>
     )
